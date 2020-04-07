5307ecd06b9693b334ee296491417d2942bda761
root <297511521@qq.com>
2020-04-07 16:01:33 +0800

    k8s

---
+++ k8s/canal.sh
@@ -0,0 +1,3 @@
+kubectl apply -f  https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/canal/rbac.yaml \
+&& kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/canal/canal.yaml
+
---
+++ k8s/master.sh
@@ -0,0 +1,32 @@
+
+if  [ ! -n "${1}" ] ;then
+  echo "you have not input the ip,input e.p:192.168.2.101/tk8s-master/n192.168.2.102/tk8s-worker"
+  exit 1
+else
+  echo "the config you input are ${1}"
+fi
+
+if [ ! -n "${2}" ];then
+  echo "master init address is null,e.p:192.168.2.101"
+  exit 1
+else
+  echo "the master addr is ${2}"
+fi
+
+echo -e "${1}" >> /etc/hosts
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted" > /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted" >> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial universe" >> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe" >> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse" >> /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb https://mirrors.aliyun.com/kubernetes/apt kubernetes-xenial main" >> /etc/apt/sources.list
+apt-get install docker.io
+wait
+apt-get update && apt-get install -y kubelet kubeadm kubectl --allow-unauthenticated
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address="${2}"
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
